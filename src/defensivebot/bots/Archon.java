0aeac0d8ee71f3a0e1d1c7f780f59f08023bf54d
Scott Poole <scotty_poole57@aol.com>
2022-01-03 21:49:09 -0800

    added helper functions to Robot. Set up Archon for Miner testing.

--- src/defensivebot/bots/Archon.java
+++ src/defensivebot/bots/Archon.java
@@ -1,15 +1,26 @@
 package defensivebot.bots;
 
-import battlecode.common.GameActionException;
-import battlecode.common.RobotController;
+import java.util.Random;
+
+import battlecode.common.*;
+import defensivebot.utils.*;
 
 public class Archon extends Robot{
     public Archon(RobotController rc) {
         super(rc);
     }
 
+    
+    static final Random rng = new Random(6147);
     @Override
     public void executeRole() throws GameActionException {
-
+    	
+    	//for testing miners
+    	//build miners in random direction whenever possible
+    	Direction dir = Constants.directions[rng.nextInt(Constants.directions.length)];
+    	rc.setIndicatorString("Trying to build a miner");
+        if (rc.canBuildRobot(RobotType.MINER, dir)) {
+            rc.buildRobot(RobotType.MINER, dir);
+        }
     }
 }
