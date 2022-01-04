6ad54736449ec0f8d02b9686f739124ce16c4e14
Aditya Chawla <adityachawla@Adityas-MacBook-Air.local>
2022-01-03 19:26:30 -0800

    updated gradle. added abstraction

---
+++ src/defensivebot/bots/Builder.java
@@ -0,0 +1,16 @@
+package defensivebot.bots;
+
+import battlecode.common.GameActionException;
+import battlecode.common.RobotController;
+
+public class Builder extends Robot{
+
+    public Builder(RobotController rc) {
+        super(rc);
+    }
+
+    @Override
+    public void executeRole() throws GameActionException {
+
+    }
+}
---
+++ src/defensivebot/bots/WatchTower.java
@@ -0,0 +1,15 @@
+package defensivebot.bots;
+
+import battlecode.common.GameActionException;
+import battlecode.common.RobotController;
+
+public class WatchTower extends Robot{
+    public WatchTower(RobotController rc) {
+        super(rc);
+    }
+
+    @Override
+    public void executeRole() throws GameActionException {
+
+    }
+}
