961ed5f0af25e5b4d4c3354d00747e12f35a7cee
Scott Poole <scotty_poole57@aol.com>
2022-01-16 18:15:20 -0800

    stops some miner spazzing

--- src/defensivebot2/bots/Miner.java
+++ src/defensivebot2/bots/Miner.java
@@ -114,7 +114,7 @@ public class Miner extends Robot{
 	}
 
 	private void tryMoveOnTask() throws GameActionException {
-		if(!rc.isMovementReady() || taskLoc == null) return;
+		if(!rc.isActionReady() || !rc.isMovementReady() || taskLoc == null) return;
 		//arrived at task
 		if(rc.getLocation().isWithinDistanceSquared(taskLoc, Constants.CLOSE_RADIUS)) {
 			taskLoc = null;
